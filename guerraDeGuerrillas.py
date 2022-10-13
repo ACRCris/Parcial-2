e861ab3528ae85f127ff573ed588ab3c4d17f681
ACRCris <crperezp@una.edu.co>
2022-10-13 16:24:05 -0500

    getAttackUniity

--- guerraDeGuerrillas.py
+++ guerraDeGuerrillas.py
@@ -1,16 +1,43 @@
 import numpy as np
 
-def getAttackUnities():
-        np.random.seed(0)
-        maxAttackStrength = 10
+def getAttackUnities0():
+
+        np.random.seed(1)
+        maxAttackStrength = 10000
         sumStrengthAttack = 0
+
         attackUnities = dict()
-        while sumStrengthAttack < maxAttackStrength:
+        while sumStrengthAttack != maxAttackStrength:
+                strengthAttack = np.random.randint(1, maxAttackStrength+1)
+                sumStrengthAttack += strengthAttack
                 while sumStrengthAttack > maxAttackStrength:
+                        sumStrengthAttack -= strengthAttack
                         strengthAttack = np.random.randint(1, maxAttackStrength+1)
+                        sumStrengthAttack += strengthAttack
+
+                if(attackUnities.get(strengthAttack) == None):
                         attackUnities[strengthAttack] = strengthAttack
+                else:
+                        attackUnities[strengthAttack] += strengthAttack
+
+        
+        return attackUnities, sumStrengthAttack
 
 
-        return attackUnities
+def  getAttackUnitiesN(attackUnities, maxAttackStrength = 10000):
+        raise NotImplementedError
 
-print(getAttackUnities())
+def getAttackUnity(attackUnities, maxAttackStrength = 10000):
+        np.random.seed(1)
+        labelsAttackUnities = np.array(list(attackUnities.keys()))
+        probablity = np.random.rand()*maxAttackStrength
+        for i in range(len(labelsAttackUnities)):
+                labela = labelsAttackUnities[i-1] if i-1>=0  else 0
+                label = labelsAttackUnities[i]
+                leftVal = attackUnities[labela] if labela != 0 else 0
+                rightVal = attackUnities[label] + leftVal
+                if (probablity < rightVal) and probablity > leftVal:
+                        return label, attackUnities.get(label)
+
+                
+
