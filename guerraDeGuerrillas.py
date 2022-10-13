d97d25736735985533fd981b4138cf98148aa860
ACRCris <crperezp@una.edu.co>
2022-10-13 18:56:23 -0500

    fusion and fision

--- guerraDeGuerrillas.py
+++ guerraDeGuerrillas.py
@@ -1,3 +1,4 @@
+from distutils.command.config import LANG_EXT
 import numpy as np
 
 def getAttackUnities0():
@@ -24,20 +25,72 @@ def getAttackUnities0():
         return attackUnities, sumStrengthAttack
 
 
-def  getAttackUnitiesN(attackUnities, maxAttackStrength = 10000):
-        raise NotImplementedError
+def  getAttackUnitiesN(attackUnities,seed, maxAttackStrength = 10000, umbralV = 0.01):
+        np.random.seed(seed)
+        labeli = getAttackUnity(attackUnities, maxAttackStrength)
+        probabilityV = np.random.rand()
+        if(labeli == 1 or probabilityV <= umbralV):
+                labelj = getAttackUnity(attackUnities, maxAttackStrength)
+                
+                while labelj == labeli:
+                        labelj = getAttackUnity(attackUnities, maxAttackStrength)
 
-def getAttackUnity(attackUnities, maxAttackStrength = 10000):
-        np.random.seed(1)
-        labelsAttackUnities = np.array(list(attackUnities.keys()))
-        probablity = np.random.rand()*maxAttackStrength
-        for i in range(len(labelsAttackUnities)):
-                labela = labelsAttackUnities[i-1] if i-1>=0  else 0
-                label = labelsAttackUnities[i]
-                leftVal = attackUnities[labela] if labela != 0 else 0
-                rightVal = attackUnities[label] + leftVal
-                if (probablity < rightVal) and probablity > leftVal:
-                        return label, attackUnities.get(label)
+                attackUnities = fusionAttackUnity(labeli, labelj, attackUnities)
+        elif(probabilityV > umbralV or labeli == maxAttackStrength):
+                attackUnities = fissionAttackUnity(labeli, attackUnities)
+
+        return attackUnities
 
                 
+
+def getAttackUnity(attackUnities, maxAttackStrength = 10000):
+        #np.random.seed(seed)
+        labelsAttackUnities = np.array(list(attackUnities.keys()))
+        probablityP = np.random.rand()*maxAttackStrength
+        rightVal = 0
+        for i in range(len(labelsAttackUnities)):
+                #labela = labelsAttackUnities[i-1] if i-1>=0  else 0
+                label = labelsAttackUnities[i]
+                leftVal = rightVal
+                rightVal = attackUnities[label] + leftVal
+                if (probablityP < rightVal) and probablityP >= leftVal:
+                        return label
+
+
+
+def fusionAttackUnity(labeli, labelj, attackUnities):
+        print("fusionAttackUnity", labeli, labelj)
+        if(attackUnities[labeli] == labeli):
+                attackUnities.pop(labeli)
+        else:
+                attackUnities[labeli] -= labeli
+
+        if(attackUnities[labelj] == labelj):
+                attackUnities.pop(labelj)
+        else:
+                attackUnities[labelj] -= labelj
+
+        if(attackUnities.get(labeli+labelj) == None):
+                attackUnities[labeli+labelj] = labeli+labelj
+        else:
+                attackUnities[labeli+labelj] += labeli+labelj
+
+        return attackUnities
+
+def fissionAttackUnity(label, attackUnities):
+        if(attackUnities[label] == label):
+                attackUnities.pop(label)
+        else:
+                attackUnities[label] -= label
+
+        if(attackUnities.get(1) == None):
+                attackUnities[1] = label
+        else:
+                attackUnities[1] += label
+
+        return attackUnities
+
+print(getAttackUnities0()[0])
+for i in range(100):
+        print(i, getAttackUnitiesN(getAttackUnities0()[0],i,umbralV = 0.5))
 
